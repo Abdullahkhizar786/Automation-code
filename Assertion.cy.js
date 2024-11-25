96800ee30cb98f98d36ce0746737c3e8bb347833
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2024-11-25 19:54:11 +0500

    new code

--- Assertion.cy.js
+++ Assertion.cy.js
@@ -1,23 +1,20 @@
 describe("Assertion pratice", () => {
-    //There are two types of assertion implicit & explicit 
-    // in implicit tow terms Should and And should is for valaditing and And is for such as or 
-    // .url with get the url of the page 
+  //There are two types of assertion implicit & explicit
+  // in implicit tow terms Should and And should is for valaditing and And is for such as or
+  // .url with get the url of the page
   it("implicit Assertion shoud and", () => {
-    cy.visit("https://www.demoblaze.com/")
-    cy.url().should('include','demoblaze.com')
-    cy.url().should('not.eq','democvnsdj')
-    cy.url().should('contain','demoblaze')
+    cy.visit("https://www.demoblaze.com/");
+    cy.url().should("include", "demoblaze.com");
+    cy.url().should("not.eq", "democvnsdj");
+    cy.url().should("contain", "demoblaze");
 
-
-    cy.url().should('not.eq','abdullah').should('contain','demo')
+    cy.url().should("not.eq", "abdullah").should("contain", "demo");
     //.title will take the title of the page dom element first.
-    cy.title().should('contain','STORE').and('eq','STORE').and('not.eq','ASDAS')
-    cy.get('#nava').should('be.visible').and('not.eq','ASAD')
-    
-
-
-    
+    cy.title()
+      .should("contain", "STORE")
+      .and("eq", "STORE")
+      .and("not.eq", "ASDAS");
+    cy.get("#nava").should("be.visible").and("not.eq", "ASAD");
+    cy.get("#cat").should("be.visible");
   });
-
-
 });
