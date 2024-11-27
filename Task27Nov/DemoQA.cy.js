523a00d9f0ee97193362a4dfc771a53a25a80136
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2024-11-27 20:52:48 +0500

    Task

---
+++ Task27Nov/DemoQA.cy.js
@@ -0,0 +1,43 @@
+
+
+//beforeEach(() => {cy.visit("https://demoqa.com/");})
+import { DemoNav } from "../../utils/demofunction"
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+    });
+describe('Demo site test',() => {
+    it('to verify that new user can enter new data in table',() =>{
+        cy.visit("https://demoqa.com/")
+        cy.url().should("include","demo")
+        cy.scrollTo('center')
+        cy.wait(3000)
+        cy.get(DemoNav.toclickonElement).eq(0).click()
+        cy.get(DemoNav.toclickwebpage).eq(3).click()
+        cy.get(DemoNav.toclickonadd).click()
+        cy.wait(5000)
+        cy.get(DemoNav.toenterfirstname).type('Abdullah')
+        cy.get(DemoNav.toenterLastname).type('Khizar')
+        cy.get(DemoNav.toenterEmail).type('Abdullahkhizar@gmail.com')
+        cy.get(DemoNav.toenterAge).type('21')
+        cy.get(DemoNav.toentersalary).type('1111')
+        cy.get(DemoNav.toenterDeprt).type('QA Automation')
+        cy.get(DemoNav.toentersummit).click()
+    
+    })
+    it.only('Verift that the user can edit',() =>{
+      cy.visit('https://demoqa.com/')
+      cy.get(DemoNav.toclickonElement).eq(0).click()
+      cy.get('[id="item-3"]').should('be.visible')
+      cy.get(DemoNav.toclickwebpage).eq(3).click()
+      cy.scrollTo('center')
+      cy.get(DemoNav.forEditing).click()
+      cy.get(DemoNav.toenterfirstname).clear().type('Completed')
+      cy.get(DemoNav.toenterLastname).clear().type('BV')
+      cy.get('#submit').should('be.visible')
+      cy.get(DemoNav.tosummitEdit).click()
+
+
+    })
+
+
+})
---
+++ Task27Nov/demofunction.js
@@ -0,0 +1,14 @@
+export const DemoNav = {
+    toclickonElement: '[class="card mt-4 top-card"]',
+    toclickwebpage: '[class="text"]',
+    toclickonadd: '#addNewRecordButton',
+    toenterfirstname: '#firstName',
+    toenterLastname: '#lastName',
+    toenterEmail: '#userEmail',
+    toenterAge: '#age',
+    toentersalary: '#salary',
+    toenterDeprt: '#department',
+    toentersummit: '#submit',
+    forEditing: '#edit-record-2',
+    tosummitEdit: '#submit',
+}
