2f5a1a8090c94a71fb2412fb36bb992ed3c543e6
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2024-12-02 18:16:40 +0500

    Task with POM Method

---
+++ TaskWithPoM method/DemoQAPOM.cy.js
@@ -0,0 +1,9 @@
+import { pOMmodel } from "../../utils/DemoQAPOM";
+describe("pomcode", () => {
+  it("PomMethod", () => {
+    pOMmodel.taskno1();
+    pOMmodel.taskno2();
+    pOMmodel.taskno3();
+    pOMmodel.taskno4();
+  });
+});
---
+++ TaskWithPoM method/demofunction.js
@@ -0,0 +1,33 @@
+export const DemoNav = {
+  toclickonElement: '[class="card mt-4 top-card"]',
+  toclickwebpage: '[class="text"]',
+  toclickonadd: "#addNewRecordButton",
+  toenterfirstname: "#firstName",
+  toenterLastname: "#lastName",
+  toenterEmail: "#userEmail",
+  toenterAge: "#age",
+  toentersalary: "#salary",
+  toenterDeprt: "#department",
+  toentersummit: "#submit",
+  forEditing: "#edit-record-2",
+  tosummitEdit: "#submit",
+  toclickonBrokenlinkimage: "#item-6",
+  toclickonFoam: '[class="card mt-4 top-card"]',
+  toclickongenderbutton: '[class="custom-control-label"]',
+  toentermobileNum: "#userNumber",
+  toenterDOB: "#dateOfBirthInput",
+  toclickinyear: '[class="react-datepicker__year-select"]',
+  toclickonmonth: '[class="react-datepicker__month-select"]',
+  toclickonday: '[class="react-datepicker__day react-datepicker__day--015"]',
+  toentersubject: "#subjectsContainer",
+  toselecthobbies: '[class="custom-control-label"]',
+  touploadimage: '[type="file"]',
+  toenteradress: "#currentAddress",
+  toselectstate: '[class=" css-tlfecz-indicatorContainer"]',
+  toselectcity: '[class=" css-tlfecz-indicatorContainer"]',
+  toselectNCR: "#react-select-3-option-0",
+  toselectDelhi: "#react-select-4-option-0",
+};
+
+
+
