afc726381ae9b1fa88852885869a00b013dbe643
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2024-12-03 20:01:50 +0500

    Task no 5,6,7

---
+++ Task no 5,6,7/demofunction.js
@@ -0,0 +1,43 @@
+export const DemoNav = {
+  toclickonElement: '[class="card mt-4 top-card"]',
+  toclickonInteractions: '[class="card mt-4 top-card"]',
+  toclickonDroppable: '[class="element-list collapse show"]>[class="menu-list"]>[id="item-3"]',
+  toclickwebpage: '[class="text"]',
+  tolickonWidgets: '[class="card mt-4 top-card"]',
+  toclickonslider: '[class="element-list collapse show"]>[class="menu-list"]>[id="item-4"]',
+  toclickonToolTips: '[class="element-list collapse show"]>[class="menu-list"]>[id="item-6"]',
+  toselecthower: '#toolTipButton',
+  // toclickonslider: '[class="btn btn-light "]',
+  toclickonstart: '#startStopButton',
+  toclickonadd: "#addNewRecordButton",
+  toenterfirstname: "#firstName",
+  toenterLastname: "#lastName",
+  toenterEmail: "#userEmail",
+  toenterAge: "#age",
+  toentersalary: "#salary",
+  toenterDeprt: "#department",
+  toentersummit: "#submit",
+  forEditing: "#edit-record-2",
+  tosummitEdit: "#submit",
+  toclickonBrokenlinkimage: "#item-6",
+  toclickonFoam: '[class="card mt-4 top-card"]',
+  toclickongenderbutton: '[class="custom-control-label"]',
+  toentermobileNum: "#userNumber",
+  toenterDOB: "#dateOfBirthInput",
+  toclickinyear: '[class="react-datepicker__year-select"]',
+  toclickonmonth: '[class="react-datepicker__month-select"]',
+  toclickonday: '[class="react-datepicker__day react-datepicker__day--015"]',
+  toentersubject: "#subjectsContainer",
+  toselecthobbies: '[class="custom-control-label"]',
+  touploadimage: '[type="file"]',
+  toenteradress: "#currentAddress",
+  toselectstate: '[class=" css-tlfecz-indicatorContainer"]',
+  toselectcity: '[class=" css-tlfecz-indicatorContainer"]',
+  toselectNCR: "#react-select-3-option-0",
+  toselectDelhi: "#react-select-4-option-0",
+};
+
+
+
+
+
