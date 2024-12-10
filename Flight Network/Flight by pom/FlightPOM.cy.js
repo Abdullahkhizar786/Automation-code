5596b8091b4bd48dc3813de7ed4c8a4a7abb7aad
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2024-12-10 19:58:48 +0500

    flight with Pom

---
+++ Flight Network/Flight by pom/FlightPOM.cy.js
@@ -0,0 +1,10 @@
+import { flightAuto } from "../../utils/FlightPOM";
+
+describe("Flight network Automation", () => {
+  it("Test case", () => {
+    flightAuto.testno1();
+    flightAuto.testno2();
+    flightAuto.testno3();
+    flightAuto.testno4();
+  });
+});
